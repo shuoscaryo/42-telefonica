52383d1d29e8571257232cb264cce9db3d1ae8a8
shuoscaryo <shuoscaryo@gmail.com>
2023-03-13 16:42:53 +0100

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:28:51 by orudek            #+#    #+#             */
-/*   Updated: 2023/03/13 16:25:43 by orudek           ###   ########.fr       */
+/*   Updated: 2023/03/13 16:42:43 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,46 @@ static int	ft_get_num_words(const char *s, char c)
 	count = 0;
 	while (*s)
 	{
-		if()
+		if (is_word && *s == ' ')
+			is_word = 0;
+		else if (!is_word && *s != ' ')
+		{
+			is_word = 1;
+			count++;
+		}
+		s++;
 	}
+	return (count);
+}
+
+char	*ft_get_word(const char **s, char c)
+{
+	size_t	i;
+	char	*out;
+	size_t	len;
+
+	len = 0;
+	i = 0;
+	while (*s[len] && *s[len] != c)
+		len++;
+	out = malloc(len + 1);
+	while (i < len)
+		out[i++] = *((*s)++);
+	(*s)++;
+	return (out);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**words;
 	size_t	num_words;
-	
+	size_t	i;
+
 	num_words = ft_get_num_words(s, c);
-	
+	words = malloc((sizeof(char *) * (num_words + 1)));
+	i = 0;
+	while (i < num_words)
+		words[i++] = ft_get_word(&s, c);
+	words[i] = 0;
+	return (words);
 }
